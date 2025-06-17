9362a73ff296d051bf492e4403ad2da2a643a828
Ed <96445749+TheShuEd@users.noreply.github.com>
2025-06-17 16:55:50 +0300

    Update ConsoleModule.cs

--- Content.Server/Discord/DiscordLink/SlashCommands/ConsoleModule.cs
+++ Content.Server/Discord/DiscordLink/SlashCommands/ConsoleModule.cs
@@ -7,10 +7,15 @@ namespace Content.Server.Discord.DiscordLink.SlashCommands;
 public sealed class ConsoleModule : InteractionModuleBase<SocketInteractionContext>
 {
     [Dependency] private readonly IConsoleHost _console = default!;
+    [Dependency] private readonly ILogManager _logManager = default!;
+
+    private ISawmill _sawmill = default!;
 
     public ConsoleModule()
     {
         IoCManager.InjectDependencies(this);
+
+        _sawmill = _logManager.GetSawmill("discord.interactions.console");
     }
 
     [SlashCommand("sudo", "Executes the specified command in the server console")]
@@ -23,11 +28,12 @@ public sealed class ConsoleModule : InteractionModuleBase<SocketInteractionConte
         try
         {
             await DeferAsync();
+            _sawmill.Info($"Discord user {Context.User.Username} sent the command to the server console: {command}");
             _console.ExecuteCommand(command);
         }
         catch
         {
-            await FollowupAsync($"Something went wrong", ephemeral: true);
+            await FollowupAsync("Something went wrong", ephemeral: true);
         }
     }
 }
